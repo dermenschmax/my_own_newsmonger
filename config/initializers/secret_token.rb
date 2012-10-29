02fad029aa9a11e0a39eb1a38d90ac95bec39971
dev <dev@development.local>
2012-10-29 20:51:56 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyOwnNewsmonger::Application.config.secret_token = 'be720a8fefc9ac8866338f009a3e65e8b59792a4a30f7eba18f1da0e19348ca01c879c7d74f2c98ccec7864ac1f1f34ecf738ea8168422e68e655a1f2591ba34'
