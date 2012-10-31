ba539e11cd0e2f0e6d7b0026e83bed274d4b91b8
dev <dev@development.local>
2012-10-31 21:30:13 +0100

    first version of Stories
    Added title, text and user_name to Stories.rb

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,29 @@
 class Story
   include MongoMapper::Document
 
+  key :title,     String
+  key :text,      String
+  
+  key :user_name, String
+  
+  
+  #key :slug,      String
+  #key :voters,    Array
+  #key :votes,     Integer, :default => 0
+  #key :relevance, Integer, :default => 0
+  #
+  ## Cached values.
+  #key :comment_count, Integer, :default => 0
+  #key :username,      String
+  #
+  ## Note this: ids are of class ObjectId.
+  #key :user_id,   ObjectId
+  #timestamps!
+  #
+  ## Relationships.
+  #belongs_to :user
+  #
+  ## Validations.
+  #validates_presence_of :title, :url, :user_id
 
 end
